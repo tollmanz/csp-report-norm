aac71ff86d34ef097aa55eea1f264cb6529fccc7
Zack Tollman <tollmanz@gmail.com>
2015-08-11 20:25:10 -0700

    Add blocked-uri tests.

--- test/blocked-uri.js
+++ test/blocked-uri.js
@@ -15,4 +15,16 @@ describe('handlers/blocked-uri', function() {
   it('converts blob URIs to blob', function() {
     sanitize('blob:xxxxxxxx').should.equal('blob');
   });
+
+  it('converts a non-URI string to an empty string', function() {
+    sanitize('test', {}).should.equal('');
+  });
+
+  it('returns the origin of a blocked URI when it does not match the protected resource\'s origin ', function() {
+    sanitize('http://www.example.com/hello-world', 'http://www.another-example.com').should.equal('http://www.example.com/');
+  });
+
+  it('returns the full blocked URI when it matches the protected resource\'s origin ', function() {
+    sanitize('http://www.example.com/hello-world', 'http://www.another-example.com').should.equal('http://www.example.com/');
+  });
 });
