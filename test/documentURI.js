69eab40afea7eeb723260c6ba3c2db80a9fa0c0e
Zack Tollman <tollmanz@gmail.com>
2015-08-20 22:28:19 -0400

    Add documentURI sanitization.

--- test/documentURI.js
+++ test/documentURI.js
@@ -8,5 +8,29 @@ suite('sanitize', function() {
     test('sanitize is a function', function() {
       assert.isFunction(sanitize);
     });
+
+    test('valid URL returns URL', function() {
+      var url = 'http://example.com';
+      assert.equal(sanitize(url), url);
+    });
+
+    test('valid URL with trailingslash returns URL', function() {
+      var url = 'http://example.com/';
+      assert.equal(sanitize(url), url);
+    });
+
+    test('invalid URL return empty string', function() {
+      assert.equal(sanitize('testing'), '');
+    });
+
+    test('IP address with protocol returns IP address with protocol', function() {
+      var ipWithProtocol = 'http://127.0.0.1';
+      assert.equal(sanitize(ipWithProtocol), ipWithProtocol);
+    });
+
+    test('IP address alone returns IP address alone', function() {
+      var ipWithoutProtocol = 'http://127.0.0.1';
+      assert.equal(sanitize(ipWithoutProtocol), ipWithoutProtocol);
+    });
   });
 });
