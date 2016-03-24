2606dddb40e48ea7a9531d401161aaf5097317f0
Zack Tollman <tollmanz@gmail.com>
2016-03-24 13:37:37 -0700

    Add get commands for statusCode and referrer

---
+++ test/get/referrer.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var assert = require('chai').assert;
+var get = require('../../lib/get/referrer').get;
+
+suite(__dirname.split('/').pop(), function() {
+  suite(__filename.split('/').pop().replace('.js', ''), function() {
+    test('get is a function', function() {
+      assert.isFunction(get);
+    });
+
+    test('returns referrer correctly when passed referrer', function() {
+      var referrer = 'http://example.com';
+      var report = {
+        'csp-report': {
+          'referrer': referrer
+        }
+      };
+
+      assert.equal(get(report), referrer);
+    });
+
+    test('returns empty string when directive is not proper', function() {
+      var referrer = 'blah http://example.com';
+      var report = {
+        'csp-report': {
+          'referrer': referrer
+        }
+      };
+
+      assert.equal(get(report), '');
+    });
+  });
+});
---
+++ test/get/statusCode.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var assert = require('chai').assert;
+var get = require('../../lib/get/statusCode').get;
+
+suite(__dirname.split('/').pop(), function() {
+  suite(__filename.split('/').pop().replace('.js', ''), function() {
+    test('get is a function', function() {
+      assert.isFunction(get);
+    });
+
+    test('returns statusCode correctly when passed statusCode', function() {
+      var statusCode = 200;
+      var report = {
+        'csp-report': {
+          'status-code': statusCode
+        }
+      };
+
+      assert.equal(get(report), statusCode);
+    });
+
+    test('returns empty string when directive is not proper', function() {
+      var statusCode = 'blah http://example.com';
+      var report = {
+        'csp-report': {
+          'status-code': statusCode
+        }
+      };
+
+      assert.equal(get(report), '');
+    });
+  });
+});
